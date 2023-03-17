333dab47fcbb36c9aa5be5204e2cdf0b324ec8bf
Cariks <eriks.cariks.lvt@gmail.com>
2023-03-17 08:24:37 +0200

    Pievienotas pogas un attēli. Izveidots Action Listener

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		PizzaOrderingSystem main_log = new PizzaOrderingSystem();
+		main_log.newFrame();
+	} 
 }
--- src/PizzaOrderingSystem.java
+++ src/PizzaOrderingSystem.java
@@ -1,9 +1,58 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class PizzaOrderingSystem {
+public class PizzaOrderingSystem  implements ActionListener{
+	JFrame frame = new JFrame("Pizza Ordering System");
+	JButton jaunaisPasutijums = new JButton("Jaunais pasûtîjums");
+	JButton apskatitPasutijumus = new JButton("Apskatît pasûtîjumus");
+	JButton apturet = new JButton("Apturçt programmu");
 
+	
+	public void newFrame() {
+		ImageIcon pizza_image = new ImageIcon("pizza_image.png");
+	    ImageIcon pizza_logo = new ImageIcon("pizza_logo.png");
+
+	    int garums = 600;
+	    int platums = 400;
+	    
+	    frame.setLayout(new FlowLayout());
+	    frame.setResizable(false);
+	    frame.setLocation(1920 / 2 - garums / 2, 1080 / 2 - platums / 2);
+	    
+	    frame.add(new JLabel(pizza_image)); // Attçls
+	    frame.add(jaunaisPasutijums); // Pasutîjums
+	    jaunaisPasutijums.addActionListener(this);
+
+	    frame.add(apskatitPasutijumus); // Apskatît
+	    apskatitPasutijumus.addActionListener(this);
+
+	    frame.add(apturet); // Apturçt
+	    apturet.addActionListener(this);
+	    
+	    frame.setIconImage(pizza_logo.getImage());
+	    frame.setSize(garums, platums);
+	    frame.setVisible(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	    
+	    
+	    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
