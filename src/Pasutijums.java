2d7dff38e81bb331c0ec32116f2fe38475bf86c4
Cariks <eriks.cariks.lvt@gmail.com>
2023-03-17 08:40:45 +0200

    Izveidots jauns Pasutijuma logs. Pievienoti ievadlodziņi vārda, tālruņa un adreses ievadīšanai

--- src/Pasutijums.java
+++ src/Pasutijums.java
@@ -1,8 +1,50 @@
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
-public class Pasutijums {
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class Pasutijums extends JFrame {
+
+	  JTextField vards = new JTextField(20);
+	  JTextField numurs = new JTextField(20);
+	  JTextField adrese = new JTextField(25);
+	  JCheckBox pasVeids = new JCheckBox("Saòemt us vietas");
+
+	  JPanel ievadLodzini = new JPanel();
+
+	  
 	public void veiktPasutijumu() {
-		
+		int garums = 750;
+	    int platums = 400;
+	    setLayout(null);
+	    
+	    ievadLodzini.setLayout(new GridLayout(6, 1));
+	    ievadLodzini.add(new JLabel("Vârds")); // loga "nosaukums"
+	    ievadLodzini.add(vards); // logs
+	    ievadLodzini.add(new JLabel("Tâlr.nr."));
+	    ievadLodzini.add(numurs);
+	    ievadLodzini.add(new JLabel("Adrese"));
+	    ievadLodzini.add(adrese);
+
+	    JPanel panel1 = new JPanel();
+	    panel1.setLayout(new FlowLayout());
+	    panel1.add(ievadLodzini);
+	    panel1.add(pasVeids);
+	    panel1.setBounds(10, 10, 300, 300);
+
+
+	    add(panel1);
+	    
+	    setVisible(true);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setSize(garums, platums);
+	    setLocation(1920 / 2 - garums / 2, 1080 / 2 - platums / 2);
+	    
+	    
 	}
 
 }
